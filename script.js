90d3089c0c3683ebc3fa1a43fedcf4a37849e41d
Christine Chan <Chan-Christine@hotmail.com>
2015-12-11 14:59:12 -0500

    Added second image

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
 	
-	$(".lightbox_link").on("click", function(){
+	$(".lightbox_link").on("click", function(e){
+		console.log(this);
+		e.preventDefault()
 
-		console.log("Hello, Hi, Welcome!");
 		$(".outer-lightbox").show();
+		$("#this-image").attr("src","images/cherry-pie.png");
 		$(".outer-lightbox").on("click", function(){	
 		$(".outer-lightbox").hide();
 	})
+})
+	$(".lightbox").on("click", function(e){
+		e.preventDefault()
+		console.log(this);
 
+		$(".outer-lightbox").show();
+		$("#this-image").attr("src","images/NYCDA-logo.jpg");
+		$(".outer-lightbox").on("click", function(){	
+		$(".outer-lightbox").hide();
+		})
 	})
 });
